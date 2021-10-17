187e540557bbc440204d86645790f43b50a2882a
PGtanuki <tanukipg2021@gmail.com>
2021-10-17 21:45:19 +0900

    abc223 bmade

---
+++ ABC223.py
@@ -0,0 +1,26 @@
+# #A
+# x = int(input())
+# print('Yes' if x >= 100 and x % 100==0 else 'No')
+
+# #B
+# import copy
+# s = input()
+# t = copy.copy(s)
+# l=[]
+# #左シフト
+# for _ in range(len(s)):
+#     l.append(s)
+#     a = s[0]
+#     s = s[1:]
+#     s += a
+#     # print(s)
+# #右シフト
+# for _ in range(len(t)):
+#     l.append(t)
+#     b = t[-1]
+#     t = t[:-1]
+#     t =  b+t
+# print(min(l))
+# print(max(l))
+
+#C
