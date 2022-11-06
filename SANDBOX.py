17ac0bdadfbb86380b9ca9c556c9a7e979b79574
PGtanuki <tanukipg2021@gmail.com>
2022-11-06 10:52:54 +0900

    done

--- SANDBOX.py
+++ SANDBOX.py
@@ -5,17 +5,3 @@ import sys
 
 sys.setrecursionlimit(100000)
 
-n,x=map(int, input().split())
-x-=1
-l=list(map(int, input().split()))
-l=list(map(lambda x:x-1,l))
-ln=[False]*n
-ln[x]=True
-tmp=l[x]
-while True:
-    if ln[tmp]==True:
-        print(sum(ln))
-        exit()
-    else:
-        ln[tmp]=True
-        tmp=l[tmp]
