29d73119ea73ac216b6bd9b1796b7cfe9d667a33
PGtanuki <tanukipg2021@gmail.com>
2023-01-06 09:34:04 +0900

    done

--- SANDBOX2.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter,defaultdict,deque
-import itertools #product
-import math
-import sys
-
-sys.setrecursionlimit(10**6)
-
-
--- SANDBOX3.py
+++
@@ -1,9 +0,0 @@
-l=['O','I','Z','E','A','S','G']
-s=input()
-ss=''
-for i in s:
-    if i in l:
-        ss+=str(l.index(i))
-    else:
-        ss+=i
-print(ss)
